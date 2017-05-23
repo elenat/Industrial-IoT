3b4f9c1925c41cd851e12ba57ae9410984b3d99a
Elena T <lena.tr@gmail.com>
2017-05-23 18:22:50 +0300

    Add files via upload

--- edge_samples/http/receiver/index.js
+++ edge_samples/http/receiver/index.js
@@ -1,1 +1,13 @@
+var http = require('http');
+var querystring = require('querystring');
 
+http.createServer(function (req, res) {
+  req.on('data', chunk => {
+    var data = querystring.parse(chunk.toString());
+    console.log(data);
+  });
+  req.on('end', () => {
+    res.writeHead(200, 'OK', {'Content-Type': 'text/html'})
+    res.end('Data received.')
+  });
+}).listen(8080);
