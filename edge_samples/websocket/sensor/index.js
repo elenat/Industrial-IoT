2020a09307ea79c6059c275689ec360aaf194a5f
Elena T <lena.tr@gmail.com>
2017-05-23 18:29:42 +0300

    Add files via upload

--- edge_samples/websocket/sensor/index.js
+++ edge_samples/websocket/sensor/index.js
@@ -1,1 +1,13 @@
+const WebSocket = require('ws');
 
+const ws = new WebSocket('ws://127.0.0.1:8080');
+
+ws.on('open', function open() {
+  ws.send('hello, server');
+});
+
+ws.on('message', function incoming(data, flags) {
+  // flags.binary will be set if a binary data is received.
+  // flags.masked will be set if the data was masked.
+  console.log('received: %s', data);
+});
