19a2e3ebdd18f3184134467a0636ee20889ecf43
Elena T <lena.tr@gmail.com>
2017-06-01 22:50:27 +0300

    Add files via upload

--- edge_samples/websocket/receiver/index.js
+++ edge_samples/websocket/receiver/index.js
@@ -1,19 +1,12 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 }, function () {
+const wss = new WebSocket.Server({port: 8080}, function () {
   console.log('Websocket server started');
 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-
-    // Broadcast to everyone else
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send('new client connected');
-      }
-    });
+    console.log('received: ', message);
   });
 
   // Send message to connected client
